d883d0b524c875b2a5ba6d004bd36f7444f84de5
stayinmeils <1220745819@qq.com>
2024-12-24 10:31:51 +0800

    feat:修改 tun

--- native/device/send.go
+++ native/device/send.go
@@ -375,8 +375,6 @@ top:
 		var elemsContainerOOO *QueueOutboundElementsContainer
 		select {
 		case elemsContainer := <-peer.queue.staged:
-			e := errors.New("write1")
-			erro.Err <- e
 			i := 0
 			for _, elem := range elemsContainer.elems {
 				elem.peer = peer
@@ -409,8 +407,6 @@ top:
 
 			// add to parallel and sequential queue
 			if peer.isRunning.Load() {
-				e := errors.New("write2")
-				erro.Err <- e
 				peer.queue.outbound.c <- elemsContainer
 				peer.device.queue.encryption.c <- elemsContainer
 			} else {
